af82f0f41311b1b863cab8eb7a1fde29b95e9350
Buttonupd <Dankariuki0101@gmail.com>
2020-06-04 12:23:59 +0300

    Added a models file which carries in it different tables, installed a package pillow and migrated the database to read current objectify

--- Eject/models.py
+++ Eject/models.py
@@ -1,3 +1,89 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Avg
+from django.db.models.signals import post_save
+import numpy as np
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    profile_photo = models.ImageField(upload_to='profiles/', null=True)
+    bio = models.CharField(max_length=240, null=True)
+    phone = models.PositiveIntegerField(default=0)
+
+
+    def create_user_profile(sender, instance, created, **kwargs):
+
+        if created:
+            Profile.objects.create(user=instance)
+
+        post_save.connect(create_user-profile, sender=User)
+
+    def save_profile(sef):
+        self.save()
+
+    @classmethod
+
+    def get_profile(cls):
+        profile = Profile.objects.all()
+        return profile
+
+class Project(models.Model):
+    posted_by = models.ForeignKey(User, null = True, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length= 100, null=True)
+    project_image = models.ImageField(upload_to = 'projects/', null=True)
+    description = models.TextField(null = True)
+    project_link = models.TextField(null = True)
+
+    @classmethod
+    def get_projects(cls):
+        projects = Project.objects.all()
+        return projects
+
+    @classmethod
+    def find_project(cls,serach_term):
+        project = Project.objects.all(title__icontains=serach_term)
+        return project
+
+
+    def design_rating(self):
+        all_content = list(map(lambda x:x.content, self.reviews.all()))
+        return np.mean(all_content)
+
+    def usability_rating(self):
+        all_usability = list(map(lambda x:x.usability, self.reviews.all()))
+        return np.mean(all_usability)
+
+    def content_rating(self):
+        all_content = list(map(lambda x:x.content, self.reviews.all()))
+        return np.mean(all_content)
+
+class Reviews(models.Model):
+    RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+
+    )
+    juror = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='reviews', null=True)
+    design = models.IntegerField(choices=RATING_CHOICES, default=0)
+    usability = models.IntegerField(choices=RATING_CHOICES, default=0)
+    content = models.IntegerField(choices=RATING_CHOICES, default=True)
+    comment = models.CharField(max_length=200, null=True)
+
+    @classmethod
+    def get_reviews(cls):
+        reviews = Reviews.objects.all()
+        return reviews
+
+
+
+
